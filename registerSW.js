f09272a9865802368fb0b02d1124ff7919f767e0
khaledh150 <khaledhamamd150@gmail.com>
2025-11-26 18:22:20 +0700

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/soroban-school/sw.js', { scope: '/soroban-school/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
